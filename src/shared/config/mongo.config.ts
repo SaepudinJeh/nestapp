ef2ba66f1194854c9cafc85ec45aeaf7cc016092
SaepudinJeh <saepudinjeh@gmail.com>
2022-10-13 23:54:57 +0700

    add procfile and fix mongo docker

--- src/shared/config/mongo.config.ts
+++ src/shared/config/mongo.config.ts
@@ -7,4 +7,6 @@ const {
   MONGO_PASSWORD,
 } = process.env;
 
-export const mongo_uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ked2wqs.mongodb.net/?retryWrites=true&w=majority`;
+export const mongo_uri: string = isDevelopment
+  ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017`
+  : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ked2wqs.mongodb.net/?retryWrites=true&w=majority`;
