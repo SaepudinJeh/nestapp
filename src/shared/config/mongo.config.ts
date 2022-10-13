d4e079359edd30ed6e18ffbca957df28c8c9d297
SaepudinJeh <saepudinjeh@gmail.com>
2022-10-14 00:08:58 +0700

    update mongo and install package

--- src/shared/config/mongo.config.ts
+++ src/shared/config/mongo.config.ts
@@ -1,12 +1,7 @@
 const isDevelopment = process.env.NODE_ENV !== 'production';
-const {
-  MONGO_PORT,
-  MONGO_HOSTNAME,
-  MONGO_DATABASE,
-  MONGO_USERNAME,
-  MONGO_PASSWORD,
-} = process.env;
+const { MONGO_PORT, MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD } =
+  process.env;
 
 export const mongo_uri: string = isDevelopment
-  ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017`
-  : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ked2wqs.mongodb.net/?retryWrites=true&w=majority`;
+  ? `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}`
+  : `mongodb+srv://wkwkwk:wkwkwk@cluster0.ked2wqs.mongodb.net/?retryWrites=true&w=majority`;
